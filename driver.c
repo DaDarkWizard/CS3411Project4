442895a5316d668541210de115b85bfbbf0f43dc
DaDarkWizard <dtmasker@mtu.edu>
2022-03-16 15:44:42 -0400

    Created TODOs in driver.c

--- driver.c
+++ driver.c
@@ -8,5 +8,11 @@
 
 int main(int argc, char** argv)
 {
+    // TODO: Create Even Process.
+    // TODO: Create Odd Process.
+    // TODO: Create Scanner Process.
+    // TODO: Pipe file input to Scanner Process.
+    // TODO: Close Scanner Process stdin.
+    // TODO: Gracefully wait for children to exit.
     return -1;
 }
