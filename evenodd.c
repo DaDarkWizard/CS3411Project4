7ff4a0a63cac2829e28fd1f71d4fb7aa6dd657d4
HyperTitian <bchodsdo@FIREBRAND.localdomain>
2022-03-29 20:06:16 -0400

    This might get entirely lost in the void

--- evenodd.c
+++ evenodd.c
@@ -6,7 +6,60 @@
  * Parses input passed in from the scanner.
  */
 
-int main(int argc, char** argv)
-{
-    return -1;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+
+	// Received input from stdin and write it to a temp file
+	FILE* fp = fopen("temp","w+");
+	char buf[1024];
+	fgets(buf,1024,stdin);
+	fputs(buf,fp);
+
+
+	// Reads temp file to determine number of string inputs
+	rewind(fp);
+	char word[1024];
+	int size = 0;
+	while(fscanf(fp,"%s",word) > 0){size++;}
+
+	// Reads string inputs into local array
+	rewind(fp);
+	int i = 0;
+	char table[size][1024];
+	while(fscanf(fp,"%s",word) > 0){
+
+		strcpy(table[i],word);
+		i++;
+	}
+
+	// Removes file pointer and temp file
+	fclose(fp);
+	remove("temp");
+
+	// Prints word size pairs to stdout
+	for(int i = 0; i < size; i++){
+
+		int len = strlen(table[i]);
+		printf("\n	%s	%d",table[i],len);
+
+	}
+
+	// Condition for even
+	if(strcmp(argv[0],"even") == 0){
+
+	}
+
+	// Condition for odd
+	else if(strcmp(argv[0],"odd") == 0){
+
+	}
+
+	return -1;
 }
